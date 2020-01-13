5d91c429a443d3f9c079be8dbe2acc46a7559833
MinorJinx <47308878+MinorJinx@users.noreply.github.com>
2020-01-12 20:32:26 -0600

    Added replyToUser column and decoded filename

--- telegramHTMLExport.py
+++ telegramHTMLExport.py
@@ -22,6 +22,7 @@ from bs4 import BeautifulSoup
 from pathlib import Path
 from tkinter import Tk
 from tkinter import filedialog
+from unidecode import unidecode
 import csv, datetime, glob, os, re, sys
 
 # Choose directory that contains the .html files
@@ -43,24 +44,24 @@ for file in sorted(glob.glob(inputDir + '/*.html'), key=lambda f: int(f[len(inpu
 	with open(file, 'r', encoding='utf-8') as f:
 		soup = BeautifulSoup(f, 'html.parser')
 		
-	# Saves 'groupName' and finds all chat messages
-	groupName = soup.find('div', {'class': 'text bold'}).text.strip()
+	# Saves decoded 'groupName' with no spaces and finds all chat messages
+	groupName = unidecode(soup.find('div', {'class': 'text bold'}).text.strip()).replace(' ', '_')
 	chatHistory = soup.find_all('div', {'class': ['message default clearfix', 'message default clearfix joined']})
 	logFile = 'telegramGroup_'+groupName+'.csv'
 	
 	# Creates log file and writes header row, then reopens for main loop below
 	if not os.path.exists(logFile):
 		writer = csv.writer(open(logFile, 'w', encoding='utf-8', newline=''))
-		writer.writerow(['message_id', 'reply_to_msg_id', 'username', 'timestamp', 'message', 'url'])
+		writer.writerow(['message_id', 'reply_to_msg_id', 'reply_to_user', 'username', 'timestamp', 'message', 'url'])
 		
 	writer = csv.writer(open(logFile, 'a', encoding='utf-8', newline=''))
 	
-	# Supports consoles that can't display non ascii characters
 	# Uses pathlib to easily truncate path names
-	try:
-		print('Group:', groupName, 'File:', Path(*Path(file).parts[-3:]))
-	except:
-		print('Unknown_Group_Name', 'File:', Path(*Path(file).parts[-3:]))
+	print('Group:', groupName, 'File:', Path(*Path(file).parts[-3:]))
+	
+	# Message and User lists to check for replyToUser
+	messageList = []
+	userList = []
 		
 	# Main loop that gathers info about each message
 	for div in chatHistory:
@@ -69,11 +70,18 @@ for file in sorted(glob.glob(inputDir + '/*.html'), key=lambda f: int(f[len(inpu
 		if messageDiv:
 			message = messageDiv.text.strip()	# strip() removes html tags
 			messageId = div.get('id')
+			messageList.append(messageId)
 			replyToDiv = div.find('div', {'class': 'reply_to details'})
 			if replyToDiv:
-				replyTo = 'message' + re.sub('.*?([0-9]*)$',r'\1', replyToDiv.select('a')[0]['href'])
+				replyToMsg = 'message' + re.sub('.*?([0-9]*)$',r'\1', replyToDiv.select('a')[0]['href'])
+				if replyToMsg in messageList:	# If reply to message was not deleted
+					replyToUser = userList[messageList.index(replyToMsg)]
+				else:
+					replyToMsg = None
+					replyToUser = None
 			else:
-				replyTo = None
+				replyToMsg = None
+				replyToUser = None
 			timestampDiv = div.find('div', {'class': 'pull_right date details'})
 			timestamp = datetime.datetime.strptime(timestampDiv.get('title'), '%d.%m.%Y %H:%M:%S')
 			urlDiv = messageDiv.find_all('a')
@@ -81,12 +89,13 @@ for file in sorted(glob.glob(inputDir + '/*.html'), key=lambda f: int(f[len(inpu
 				if a['href']:
 					urls.append(a['href'])
 			usernameDiv = div.find('div', {'class': 'from_name'})
-			if not len(div.get('class')) == 4:
+			if not len(div.get('class')) == 4:	# If not class='message default clearfix joined'
 				username = usernameDiv.text.strip()
+			userList.append(username)
 				
-			writer.writerow([messageId, replyTo, username, timestamp, message, ','.join(map(str, urls))])
+			writer.writerow([messageId, replyToMsg, replyToUser, username, timestamp, message, ','.join(map(str, urls))])
 
 # Runs 'urlValidator.py' with arguments -c and -f
 if urlValidate:
 	print('\nValidating URLs...\n')
-	os.system(sys.executable + ' urlValidator.py -c 5 -f ' + logFile)
+	os.system(sys.executable + ' urlValidator.py -c 6 -f ' + '"' + logFile + '"')
